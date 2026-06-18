80ea7c3e5ef02daa0cb22708b1446be492b3495a
Zalan Toth <zlaval@gmail.com>
2026-06-18 23:15:03 +0200

    remove

--- event-stream-modulith/src/main/kotlin/com/zlrx/eventstreammodulith/order/Order.kt
+++
@@ -1,9 +0,0 @@
-package com.zlrx.eventstreammodulith.order
-
-import java.util.*
-
-data class Order(
-    val id: String = UUID.randomUUID().toString(),
-    val price: Int,
-    val name: String
-)
